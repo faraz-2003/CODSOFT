d97a6c2921eb640f42a7cfbb8136ed18fe606403
Mohammed Faraz <mohammedfarazmandya@gmail.com>
2024-08-21 22:08:22 +0530

    changes to the task1

--- task1.cpp
+++ task1.cpp
@@ -1,15 +1,18 @@
-#include <stdio.h>
+#include<iostream>
+#include<cstdlib>
 
 int main()
-{
-    int target=0;
+{   
+    printf("Welcome to the Number Guess!!!!!!\t(The Range is 0 to 99)\n");
+    srand((unsigned) time(NULL));
+    int target=rand()%100;
     bool flag=true;
-    printf("Enter the number to be guessed : ");
-    scanf("%d",&target);
+    int count = 0;
     while(flag){
         int guess=0;
         printf("Enter your guess : ");
         scanf("%d",&guess);
+        count=count+1;
         if(guess>target){
             printf("Too high\n");
         }
@@ -17,8 +20,9 @@ int main()
             printf("Too low\n");
         }
         else{
-            printf("Correct\n");
+            printf("\nYes you are Correct\t You took %d guesses \nCongragulations!!!!!!!\n\nThank You",count);
             flag=false;
         }
     }
-    }
+    return 0;
+}
